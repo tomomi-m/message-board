d9ced5815cb8b075d350e0a97b917aa08f924c34
tomomi-m <tomomi-m@wink.dcns.ne.jp>
2016-09-11 15:36:26 +0900

    fix invalid sitemap xml

--- lr4/app/views/site/siteIndex.blade.php
+++ lr4/app/views/site/siteIndex.blade.php
@@ -1,10 +1,10 @@
 <?php print '<?xml version="1.0" encoding="UTF-8" ?>'; ?>
 
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
- <sitemap>
 @foreach ($pages as $page)
+ <sitemap>
   <loc>{{{Request::root()}}}/site/{{{$site->id}}}/{{{$page->id}}}/get-sitemap</loc>
   <lastmod>{{{(new DateTime($page->lastMessage_at))->format(DateTime::ATOM)}}}</lastmod>
-@endforeach
  </sitemap>
+@endforeach
 </sitemapindex>
